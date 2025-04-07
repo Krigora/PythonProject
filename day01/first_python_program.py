b7217f4372f1e61be0b21132d20b4ea4659fac0b
ihorkryvynskyy <NEWlife20!>
2025-04-07 18:30:00 -0500

    create new project

---
+++ day01/first_python_program.py
@@ -0,0 +1,12 @@
+print('Python Programming')
+
+# This is a comment
+
+"""
+Multi 
+Line
+Comments
+"""
+
+print(" i love \"Python\"")
+
---
+++ day01/variables.py
@@ -0,0 +1,15 @@
+"""
+JAva variable or in static typed programming languages:
+string varNam = data
+
+"""
+
+name_of_student = "Elena"
+print(name_of_student)
+
+prog_lang = "Python"
+version = 3.13
+
+print("My prog language is " + prog_lang)
+print("My prog language is {} but versionn is {}".format(prog_lang, version))
+print(f"My prog language is {prog_lang} but versionn is {version}")
